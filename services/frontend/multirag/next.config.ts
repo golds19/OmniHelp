17b5287f38152efa7df1273b610a5f1ba225e8de
golds19 <gsbensaint19@gmail.com>
2026-03-02 09:16:46 +0900

    fix/ pin packages version and fix docker issues

--- services/frontend/multirag/next.config.ts
+++ services/frontend/multirag/next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  // Enable standalone output for Docker deployment
-  output: 'standalone',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
